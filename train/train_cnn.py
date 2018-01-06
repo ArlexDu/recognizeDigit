9759d73633fd62ca486f527a80fb97ad7d598481
Altynai <772773671@qq.com>
2018-01-06 14:15:10 +0800

    更新cnn分类器

--- train/train_cnn.py
+++ train/train_cnn.py
@@ -4,6 +4,7 @@ from network3 import ConvPoolLayer, FullyConnectedLayer, SoftmaxLayer
 training_data, validation_data, test_data = network3.load_data_shared()
 mini_batch_size = 10
 from network3 import ReLU
+load_file ='F:\\Projects\\Python\\recognizeDigit\\train\\cnn_network.json'
 net = Network([
     ConvPoolLayer(image_shape=(mini_batch_size, 1, 28, 28),
                   filter_shape=(20, 1, 5, 5),
@@ -14,6 +15,6 @@ net = Network([
                   poolsize=(2, 2),
                   activation_fn=ReLU),
     FullyConnectedLayer(n_in=40*4*4, n_out=100, activation_fn=ReLU),
-    SoftmaxLayer(n_in=100, n_out=10)], mini_batch_size)
-net.SGD(training_data, 1, mini_batch_size, 0.03, validation_data, test_data, lmbda=0.1)
+    SoftmaxLayer(n_in=100, n_out=10)], mini_batch_size,load_file)
+net.SGD(training_data, 60, mini_batch_size, 0.03, validation_data, test_data, lmbda=0.1)
 net.save("cnn_network.json")
